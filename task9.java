3290e23d669d70b5bc8361fe01825ae63ee9db1d
Anonnb666 <1760107928@qq.com>
2024-10-14 10:36:54 +0800

    Update task9.java

--- task9.java
+++ task9.java
@@ -4,7 +4,7 @@ public class task9 {
     public static void main(String[] args) {
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                if(i<=j)
+                if(i>=j)
                     System.out.print("* ");
                 else
                     System.out.print("  ");
